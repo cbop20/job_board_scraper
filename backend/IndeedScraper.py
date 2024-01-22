6158c65e23eeec7463f8c82c343ccf3d3bc7c959
cbop20 <calebstarkey@hotmail.com>
2024-01-22 08:28:11 -0700

    Sql integration complete

--- backend/IndeedScraper.py
+++ backend/IndeedScraper.py
@@ -1,34 +1,45 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-from opportunity import Opportunity
-url = "https://www.indeed.com/jobs?q=software&l=Denver%2C+CO&sc=0kf%3Aexplvl%28ENTRY_LEVEL%29%3B&radius=15&vjk=6da7a4cdaf49c7c8"
-options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
-driver = webdriver.Chrome(options=options)
-driver.get(url)
-opportunities = []
-while True:
-    the_big_cheeses = driver.find_elements(By.CLASS_NAME,'resultContent')
-    for cheese in the_big_cheeses:
-        link_title = cheese.find_element(By.XPATH,'.//a[starts-with(@aria-label,"full details")]')
-        link = link_title.get_attribute('href')
-        title = link_title.find_element(By.XPATH,'./span').text
-        company = cheese.find_elements(By.XPATH,'.//span[@data-testid="company-name"]')
-        if(len(company)>0):
-            company = company[0].text
-        else:
-            company = ""
-        job_info_elements = cheese.find_elements(By.XPATH, './/div[@data-testid="attribute_snippet_testid"]')
-        opportunities.append(Opportunity(company,title,link,[info.text for info in job_info_elements]))
-    for o in opportunities:
-        print(o)
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+import os
+def IndeedScrape(db_connection,db_cursor, headless, insert_script):
+    print("Starting Indeed scrape.")
     try:
-        next_page_link = driver.find_element(By.XPATH,'//a[@aria-label="Next Page"]')
-        driver.execute_script('arguments[0].click();', next_page_link)
-    except:
-        break
-driver.quit()
-print("Done")
+        url = os.getenv('INDEED_LINK')
+        options = webdriver.ChromeOptions()
+        if(headless):
+            options.add_argument('--headless')
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+    except Exception as ex:
+        print("Getting initial link failed.")
+        return
+    
+    while True:
+        the_big_cheeses = driver.find_elements(By.CLASS_NAME,'resultContent')
+        for cheese in the_big_cheeses:
+            link_title = cheese.find_element(By.XPATH,'.//a[starts-with(@aria-label,"full details")]')
+            link = link_title.get_attribute('href')
+            title = link_title.find_element(By.XPATH,'./span').text
+            company = cheese.find_elements(By.XPATH,'.//span[@data-testid="company-name"]')
+            if(len(company)>0):
+                company = company[0].text
+            else:
+                company = ""
+
+            job_info_elements = cheese.find_elements(By.XPATH, './/div[@data-testid="attribute_snippet_testid"]')
+            job_info = []
+            for i in job_info_elements:
+                info = i.text
+                if info:
+                    job_info.append(info)
+            
+            db_cursor.execute(insert_script, (company,title,link,job_info))
+        try:
+            db_connection.commit()
+            next_page_link = driver.find_element(By.XPATH,'//a[@aria-label="Next Page"]')
+            driver.execute_script('arguments[0].click();', next_page_link)
+        except:
+            break
+    driver.quit()
+    print("Indeed scraper done")
--- init.sql
+++ init.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS opportunities (
     id SERIAL PRIMARY KEY,
     company_name VARCHAR(255),
     title VARCHAR(255),
-    link VARCHAR(255),
+    link TEXT,
     info TEXT[]
     );
