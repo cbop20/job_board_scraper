3dbcaeb421a1f5bdde7c59ff795d8c60a23dee3c
cbop20 <calebstarkey@hotmail.com>
2024-01-22 10:04:16 -0700

    Try to get rid of some common code with a class structure. Also fixed an issue where Indeed in headless wasn't getting any data

--- backend/BuiltInCoScraper.py
+++ backend/BuiltInCoScraper.py
@@ -1,47 +1,48 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from Scraper import Scraper
 import os
-def BuiltInScrape(db_connection,db_cursor, headless, insert_script):
-    print("Starting BuiltIn scrape.")
-    try:
-        url = os.getenv('BUILTIN_LINK')
-        options = webdriver.ChromeOptions()
-        if(headless):
-            options.add_argument('--headless')
-        driver = webdriver.Chrome(options=options)
-        driver.get(url)
-    except Exception as ex:
-        print("Getting initial link failed.")
-        return
 
-    wait = WebDriverWait(driver, 10)
-
-    while True:
-        the_big_cheeses = driver.find_elements(By.XPATH, ('//div[@data-id="job-card"]'))
-        for cheese in the_big_cheeses:
-            id_company_parent = cheese.find_element(By.XPATH,('.//div[@data-id="company-title"]'))
-            id = id_company_parent.get_attribute('data-builtin-track-job-id')
-            drop_data = driver.find_element(By.XPATH,(f".//div[@data-job-id='{id}']"))
-            company = id_company_parent.find_element(By.XPATH,('./span')).text
-            job_info_elements = drop_data.find_elements(By.XPATH,('.//span'))
-            job_info = []
-            for i in job_info_elements:
-                info = i.text
-                if info:
-                    job_info.append(info)
-            
-            title_href_parent = cheese.find_element(By.XPATH,('.//a[@id="job-card-alias"]'))
-            title = title_href_parent.text
-            link = title_href_parent.get_attribute('href')
-            db_cursor.execute(insert_script, (company,title,link,job_info))
+class BuiltInScrape(Scraper):
+    def __init__(self,db_con,db_cursor,headless,insert_script):
+        super().__init__(db_con,db_cursor,headless,insert_script)
+    
+    def scrape(self):
+        print("Starting BuiltIn scrape.")
         try:
-            db_connection.commit()
-            next_page_link = driver.find_element(By.XPATH,'//a[@aria-label="Go to Next Page"]')
-            driver.execute_script('arguments[0].click();', next_page_link)
-        except:
-            break
-        
-    driver.quit()
-    print("BuiltInCo scraper done")
+            url = os.getenv('BUILTIN_LINK')
+            self.driver.get(url)
+        except Exception as ex:
+            print("Getting initial link failed.")
+            return
+
+        wait = WebDriverWait(self.driver, 10)
+
+        while True:
+            the_big_cheeses = self.driver.find_elements(By.XPATH, ('//div[@data-id="job-card"]'))
+            for cheese in the_big_cheeses:
+                id_company_parent = cheese.find_element(By.XPATH,('.//div[@data-id="company-title"]'))
+                id = id_company_parent.get_attribute('data-builtin-track-job-id')
+                drop_data = self.driver.find_element(By.XPATH,(f".//div[@data-job-id='{id}']"))
+                company = id_company_parent.find_element(By.XPATH,('./span')).text
+                job_info_elements = drop_data.find_elements(By.XPATH,('.//span'))
+                job_info = []
+                for i in job_info_elements:
+                    info = i.text
+                    if info:
+                        job_info.append(info)
+                
+                title_href_parent = cheese.find_element(By.XPATH,('.//a[@id="job-card-alias"]'))
+                title = title_href_parent.text
+                link = title_href_parent.get_attribute('href')
+                self.db_cursor.execute(self.insert_script, (company,title,link,job_info))
+            try:
+                self.db_con.commit()
+                next_page_link = self.driver.find_element(By.XPATH,'//a[@aria-label="Go to Next Page"]')
+                self.driver.execute_script('arguments[0].click();', next_page_link)
+            except:
+                break
+            
+        self.driver.quit()
+        print("BuiltInCo scraper done")
--- backend/YCombScraper.py
+++ backend/YCombScraper.py
@@ -1,66 +1,60 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from Scraper import Scraper
 import time
-import configparser
 import os
 
-def YCombScraper(db_connection,db_cursor, headless, insert_script):
-    print("Starting YComb scrape.")
-    options = webdriver.ChromeOptions()
-    if(headless):
-        options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
-    login_link = os.get("Y_COMB_LOGIN_LINK")
+class YCombScrape(Scraper):
+    def __init__(self,db_con,db_cursor,headless,insert_script):
+        super().__init__(db_con,db_cursor,headless,insert_script)
+        self.login_link = os.getenv('Y_COMB_LOGIN_LINK')
+        self.links = os.getenv('Y_COMB_LINKS', '').split(',')
+        self.username = os.getenv('Y_COMB_EMAIL')
+        self.password = os.getenv('Y_COMB_PASSWORD')
+    def scrape(self):
+        print("Starting YComb scrape.")
+        
+        self.driver.get(self.login_link)
 
-    state_link = os.get("Y_COMB_STATE_LINK")
-    remote_link = os.get("Y_COMB_REMOTE_LINK")
-    links = [state_link,remote_link]
-    
-    driver.get(login_link)
+        user_atrib = 'ycid-input'
+        password_atrib = 'password-input'
+        username_input = self.driver.find_element(By.ID, user_atrib)
+        password_input = self.driver.find_element(By.ID, password_atrib) 
+        username_input.send_keys(self.username)
+        password_input.send_keys(self.password)
 
-    config = configparser.ConfigParser()
-    config.read('../.config.ini')
-
-    user_atrib = 'ycid-input'
-    password_atrib = 'password-input'
-    username_input = driver.find_element(By.ID, user_atrib)
-    password_input = driver.find_element(By.ID, password_atrib) 
-    username_input.send_keys(os.getenv('Y_COMB_EMAIL'))
-    password_input.send_keys(os.getenv('Y_COMB_PASSWORD'))
-    login = driver.find_element(By.XPATH, '//button[.//span[text()="Log in"]]')
-    login.click()
-    try:
-        WebDriverWait(driver,10).until(
-            EC.presence_of_element_located((By.ID,'waas-sidebar-filters'))
-        )
-    except TimeoutError:
-        print("Timed out waiting for login load")
-
-    for link in links:
-        driver.get(link)
-        wait = WebDriverWait(driver, 10)
-        time.sleep(5)
-        last_height = driver.execute_script("return document.body.scrollHeight")
-        while True:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(3)
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
-        the_medium_cheese = driver.find_elements(By.XPATH,'//div[@class="mb-4 flex flex-col justify-between sm:flex-row"]')
-        for cheese in the_medium_cheese:
-            title_link = cheese.find_element(By.XPATH,'.//a')
-            title = title_link.text
-            link = title_link.get_attribute('href')
-            job_info_elements = cheese.find_elements(By.XPATH,'.//div[@class="mr-2 text-sm sm:mr-3 sm:flex sm:flex-wrap"]//span')
-            job_info = []
-            for i in job_info_elements:
-                info = i.text
-            if info:
-                job_info.append(info)
-            db_cursor.execute(insert_script, (None,title,link,job_info))
-        db_connection.commit()
-    driver.quit()
+        login = self.driver.find_element(By.XPATH, '//button[.//span[text()="Log in"]]')
+        login.click()
+        try:
+            WebDriverWait(self.driver,10).until(
+                EC.presence_of_element_located((By.ID,'waas-sidebar-filters'))
+            )
+        except TimeoutError:
+            print("Timed out waiting for login load")
+        
+        for link in self.links:
+            self.driver.get(link)
+            time.sleep(5)
+            last_height = self.driver.execute_script("return document.body.scrollHeight")
+            while True:
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(3)
+                new_height = self.driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    break
+                last_height = new_height
+            the_medium_cheese = self.driver.find_elements(By.XPATH,'//div[@class="mb-4 flex flex-col justify-between sm:flex-row"]')
+            for cheese in the_medium_cheese:
+                title_link = cheese.find_element(By.XPATH,'.//a')
+                title = title_link.text
+                link = title_link.get_attribute('href')
+                job_info_elements = cheese.find_elements(By.XPATH,'.//div[@class="mr-2 text-sm sm:mr-3 sm:flex sm:flex-wrap"]//span')
+                job_info = []
+                for i in job_info_elements:
+                    info = i.text
+                if info:
+                    job_info.append(info)
+                self.db_cursor.execute(self.insert_script, (None,title,link,job_info))
+            self.db_con.commit()
+        self.driver.quit()
