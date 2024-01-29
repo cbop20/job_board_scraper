6a21e6dd8dd2084a4ed2d0f9470f69a9ae231faa
cbop20 <calebstarkey@hotmail.com>
2024-01-29 16:49:16 -0700

    Basic search functionality complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,78 @@
-import React from 'react';
+import {React, useEffect, useState} from 'react';
 import FilterBar from './components/filterBar';
 import JobCards from './components/jobCards';
 import Button from '@mui/material/Button';
 import './styles/App.css';
+import debounce from 'lodash/debounce';
 
 function App() {
-  const [data, setData] = React.useState([]);
-  const call=()=>{
+  const [data, setData] = useState([]);
+  const [searchedJobs, setSearchedJobs] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [scrapeButton, setScrapeButton] = useState(false);
+
+  const getJobs=()=>{
     fetch('http://localhost:8080/opportunities')
-    .then(response => response.json())
-    .then(json=>setData(json))
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json()
+    })
+    .then(json=>{
+      if(json){
+        setData(json)
+        setSearchedJobs(json)
+      }
+    })
     .catch(er=>console.log(er))
  }
-  const func= async ()=> {
+
+  const scrape= async ()=> {
+    setScrapeButton(true)
     await fetch('http://localhost:5000/get_opportunities',{
       method: 'GET',
     })
-    .then(ret=>console.log(ret))
-    .catch(er=>console.log(er))
+    .then(response => {
+      setScrapeButton(false)
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+    })
+    .then(()=>{
+      getJobs()
+    })
+  
+    .catch(er=>{
+      setScrapeButton(false)
+      console.log(er)})
   };
 
-  React.useEffect(()=>{
-    call()
-  },[])
+  useEffect(() => {
+    getJobs();
+  }, []);
+
+  const search = () => {
+    const filteredItems = data.filter((job) =>
+    job.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setSearchedJobs(filteredItems);
+  }
+
+  useEffect(() => {
+    if (searchValue!= ''){
+      search()
+    }
+  },[searchValue]);
+
 
   return (
     <div className="page">
-      <Button onClick={()=>func()}>Scrape</Button>
-      <FilterBar/>
-      <JobCards {...data}/>
+      <div style={{display:'flex',justifyContent:'center',marginTop:'5px'}}>
+        <Button centerRipple className='scrape-button' onClick={()=>scrape()} disabled={scrapeButton}>Scrape</Button>
+        <FilterBar searchValue={searchValue} setSearchValue={setSearchValue} search={search} />
+      </div>
+      <JobCards {...searchedJobs}/>
     </div>
   );
 }
